b9afde6497f9c110bd68b7363e699cbed94ef5a1
Timofeychick <androidwalletmot@gmail.com>
2022-11-23 11:29:35 +0700

    Seminar_1

---
+++ Seminar_1_C#/task_3/task_3.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваше число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Ваше число четное");
+}
+else
+{
+    Console.WriteLine("Ваше число нечетное");
+}
---
+++ Seminar_1_C#/task_4/task_4.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите ваше число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+int c = 0;
+if (a % 2 == 0)
+{
+    c = a / b;
+    c = c + 1;
+    while (c < a + 1)
+    {
+        Console.Write(c);
+        Console.Write(", ");
+        c = c + 2;
+    }
+}
+else
+{
+    c = a / b;
+    c = c + 1;
+    while (c < a)
+    {
+        Console.Write(c);
+        Console.Write(", ");
+        c = c + 2;
+    }
+}
