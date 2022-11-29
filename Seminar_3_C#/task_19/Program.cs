05e6448074b61ea2a81d614701d7249e6f5ace15
Timofeychick <androidwalletmot@gmail.com>
2022-11-29 16:40:30 +0700

    решил пару задачек

--- Seminar_3_C#/task_19/Program.cs
+++ Seminar_3_C#/task_19/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Введите целое число и программа проверит его на палиндром:");
 
 int us_number = Convert.ToInt32(Console.ReadLine());
-if (us_number > 0) {
+if (us_number > 9) {
     string conv_s = us_number.ToString();
     char[] array = conv_s.ToCharArray();
     Array.Reverse(array);
@@ -17,6 +17,15 @@ if (us_number > 0) {
         Console.WriteLine("Это число - не палиндром");
     }
 }
+
+else if (us_number == 0) {
+    Console.WriteLine("Это ноль, просто ноль :)");
+}
+
+else if (us_number > -10 && us_number < 10) {
+    Console.WriteLine("Это просто одна цифорка, а не число :)");
+}
+
 else {
     us_number = us_number * -1;
     string conv_s = us_number.ToString();
