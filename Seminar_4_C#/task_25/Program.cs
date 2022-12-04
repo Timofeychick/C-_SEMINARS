09e659526663dca78f32f7d2bc40e46e35d31d06
Timofeychick <androidwalletmot@gmail.com>
2022-12-04 18:29:47 +0700

    выполнил задачки к 4-му семинару

---
+++ Seminar_4_C#/task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Напишите какое число нужно возвети в стпень:");
+int us_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите в какую стпень нужно возвести число");
+int rate = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+for (int i = 1; i < rate + 1; i++)
+{
+    result = result*us_number;
+}
+Console.WriteLine(result);
---
+++ Seminar_4_C#/task_29/Program.cs
@@ -0,0 +1,13 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+int [] array = new int [8];
+
+int len = array.Length;
+
+for (int i = 0; i < len; i++) 
+{
+    array[i] = new Random().Next(0,100);
+    Console.Write($"{array[i]} ");
+}
+
+
