c1eda9f7950a1ee8b05a68bdb9e45f3bb74ba667
Timofey_Frolov <90319573+Timofeychick@users.noreply.github.com>
2023-03-08 13:51:17 +0700

    Correct mistakes

--- Final_work_C#/task/Program.cs
+++ Final_work_C#/task/Program.cs
@@ -5,26 +5,26 @@
 
 System.Console.WriteLine("Введите размер массива");
 int sizeArr = Convert.ToInt32(Console.ReadLine());
-string[] arrey = new string[sizeArr];
+string[] array = new string[sizeArr];
 var rand = new Random();
-for (int i = 0; i < arrey.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
     int stringSize = rand.Next(1, 5);
     for (int j = 0; j < stringSize; j++)
     {
-        arrey[i] += (char)rand.Next('a', 'z');
+        array[i] += (char)rand.Next('a', 'z');
     }
 }
-System.Console.WriteLine("[" + string.Join(", ", arrey) + "]");
+System.Console.WriteLine("[" + string.Join(", ", array) + "]");
 int sizeSecondArr = 0;
 string[] arrThreeSize = new string[sizeSecondArr];
 for (int i = 0; i < arrey.Length; i++)
 {
-    if (arrey[i].Length == 3)
+    if (array[i].Length == 3)
     {
         sizeSecondArr++;
         Array.Resize(ref arrThreeSize, sizeSecondArr);
-        arrThreeSize[sizeSecondArr - 1] = arrey[i];
+        arrThreeSize[sizeSecondArr - 1] = array[i];
     }
 }
 System.Console.WriteLine("[" + string.Join(", ", arrThreeSize) + "]");
